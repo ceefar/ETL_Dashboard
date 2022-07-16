98ff687c29f3a8f3edeaf1d857b355f39876194a
Ceefar <rob.farq@googlemail.com>
2022-07-16 11:34:54 +0100

    Base Commit

---
+++ db_integration.py
@@ -0,0 +1,37 @@
+# ---- imports ----
+ 
+# for db
+import pymysql
+import os
+from dotenv import load_dotenv
+
+# load environment variables from .env file
+load_dotenv()
+host = os.environ.get("mysql_host")
+user = os.environ.get("mysql_user")
+password = os.environ.get("mysql_pass")
+database = os.environ.get("mysql_db")
+
+# establish a database connection
+connection = pymysql.connect(
+    host = host,
+    user = user,
+    password = password,
+    database = database
+)
+
+def add_to_db(command):
+    """ gets stuff from a db """
+    cursor = connection.cursor()
+    cursor.execute(f"{command}") 
+    connection.commit()
+
+def get_from_db(command):
+    """ gets stuff from a db, returns the result """
+    cursor = connection.cursor()
+    cursor.execute(f"{command}") 
+    myresult = cursor.fetchall()
+    connection.commit()
+    return(myresult)
+
+# ---- end setup ----
